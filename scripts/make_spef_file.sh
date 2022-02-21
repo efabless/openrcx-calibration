6f2ce66f6c31b1a7b0b16b7297ee22e4a1d3e872
Tim Edwards <tim@opencircuitdesign.com>
2022-02-21 12:54:09 -0500

    Extended the make_spef_file.sh script to default to sky130B but accept PDK_ROOT and PDK from the environment;  and to take a second argument which is the corner and may be "nom" (default), "min", or "max".

--- scripts/make_spef_file.sh
+++ scripts/make_spef_file.sh
@@ -7,26 +7,47 @@
 #
 # Usage:
 #
-#	make_spef_file.sh <DEF_file>
+#	make_spef_file.sh <DEF_file> [<corner>]
 
-if test "$#" -ne 1 ; then
-    echo "Usage: make_spef_file.sh <def_file_name>"
+if test "$#" -eq 0 ; then
+    echo "Usage: make_spef_file.sh <def_file_name> [<corner>]"
+    echo "    where <corner> is 'nom' (default), 'min', or 'max'".
     exit 0
 fi
 
-export PDK_ROOT=/usr/share/pdk
-export PDK_PATH=${PDK_ROOT}/sky130A
+# Default extraction style is nominal
+extstyle = 'ngspice()'
+corner = "nominal"
+
+if test "$#" -eq 2 ; then
+    corner = $2
+    if test "$2" -eq "min" ; then
+	extstyle = "ngspice\(lrlc\)"
+        corner = "minimum"
+    elif test "$2" -eq "max" ; then
+	extstyle = "ngspice\(hrhc\)"
+        corner = "maximum"
+    fi
+fi
+
+#
+# These values can be overridden from the environment
+#
+export PDK_ROOT=${PDK_ROOT:=/usr/share/pdk}
+export PDK=${PDK:=sky130B}
+export PDK_PATH=${PDK_ROOT}/${PDK}
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 designname=`cat $1 | grep ^DESIGN | cut -d' ' -f 2`
 
-echo "Extracting design $designname from $1 using magic"
-magic -dnull -noconsole -rcfile ${PDK_PATH}/libs.tech/magic/sky130A.magicrc << EOF
+echo "Extracting design $designname from $1 using magic, $corner corner"
+magic -dnull -noconsole -rcfile ${PDK_PATH}/libs.tech/magic/${PDK}.magicrc << EOF
 drc off
 crashbackups stop
 lef read ${PDK_PATH}/libs.ref/sky130_fd_sc_hd/techlef/sky130_fd_sc_hd.tlef
 def read $1
+extract style $extstyle
 extract do aliases
 extract do local
 extract all
